26be9aa0a56d3dd9de723f5f2edfbb19572335b4
VitalijF <vitalijfedyna@gmail.com>
2016-12-28 20:46:54 +0200

    Remove empty line

--- src/Server.java
+++ src/Server.java
@@ -1,7 +1,9 @@
-import com.sun.org.apache.xpath.internal.SourceTree;
-
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Server {
 
