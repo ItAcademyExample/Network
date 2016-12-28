ca9c088aa8edd571597dba19d94386d1e9d0801c
VitalijF <vitalijfedyna@gmail.com>
2016-12-28 20:41:43 +0200

    add try with resources

--- src/Client.java
+++ src/Client.java
@@ -5,36 +5,29 @@ public class Client {
   public static void main(String[] args) throws IOException {
 
     System.out.println("Welcome to Client side");
-    Socket fromServer;
-
     if (args.length == 0) {
       System.out.println("use: client hostname");
       System.exit(-1);
     }
-
     System.out.println("Connecting to... " + args[0]);
-    fromServer = new Socket(args[0], 4444);
-    BufferedReader serverInput = new BufferedReader(new InputStreamReader(fromServer.getInputStream()));
-    PrintWriter out = new PrintWriter(fromServer.getOutputStream(), true);
-    BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in));
-    String clientMessage, serverMessage;
-
-    while (true) {
-      System.out.println("New iteration Client");
-      clientMessage = consoleInput.readLine();
-      out.println(clientMessage);
-      serverMessage = serverInput.readLine();
-      System.out.println(serverMessage);
-      if (clientMessage.equalsIgnoreCase("close")) {
-        break;
-      }
-      if (clientMessage.equalsIgnoreCase("exit")) {
-        break;
+    try (Socket fromServer = new Socket(args[0], 4444);
+         BufferedReader serverInput = new BufferedReader(new InputStreamReader(fromServer.getInputStream()));
+         PrintWriter out = new PrintWriter(fromServer.getOutputStream(), true);
+         BufferedReader consoleInput = new BufferedReader(new InputStreamReader(System.in))) {
+      String clientMessage, serverMessage;
+      while (true) {
+        System.out.println("New iteration Client");
+        clientMessage = consoleInput.readLine();
+        out.println(clientMessage);
+        serverMessage = serverInput.readLine();
+        System.out.println(serverMessage);
+        if (clientMessage.equalsIgnoreCase("close")) {
+          break;
+        }
+        if (clientMessage.equalsIgnoreCase("exit")) {
+          break;
+        }
       }
     }
-    out.close();
-    serverInput.close();
-    consoleInput.close();
-    fromServer.close();
   }
 }
--- src/Server.java
+++ src/Server.java
@@ -9,14 +9,12 @@ public class Server {
     System.out.println("Welcome to Server side");
     ServerSocket servers = null;
     Socket fromclient = null;
-
     try {
       servers = new ServerSocket(4444);
     } catch (IOException e) {
       System.out.println("Couldn't listen to port 4444");
       System.exit(-1);
     }
-
     try {
       System.out.print("Waiting for a client...");
       fromclient = servers.accept();
@@ -26,30 +24,28 @@ public class Server {
       System.exit(-1);
     }
 
-    BufferedReader in = new BufferedReader(new
-        InputStreamReader(fromclient.getInputStream()));
-    PrintWriter clientOut = new PrintWriter(fromclient.getOutputStream(), true);
-    String input;
-
-    System.out.println("Wait for messages");
-    while (true) {
-      System.out.println("New Iteration");
-      input = in.readLine();
-      if (input.equalsIgnoreCase("2+2")) {
+    try(BufferedReader in = new BufferedReader(new InputStreamReader(fromclient.getInputStream()));
+        PrintWriter clientOut = new PrintWriter(fromclient.getOutputStream(), true)) {
+      String input;
+      System.out.println("Wait for messages");
+      while (true) {
+        System.out.println("New Iteration");
+        input = in.readLine();
+        if (input == null) {
+          break;
+        }
+        if (input.equalsIgnoreCase("2+2")) {
+          System.out.println("From Client: " + input);
+          clientOut.println("Echo: " + 4);
+          continue;
+        }
+        if (input.equalsIgnoreCase("exit")) {
+          break;
+        }
         System.out.println("From Client: " + input);
-        clientOut.println("Echo: " + 4);
-        continue;
+        clientOut.println("Echo: " + input);
       }
-      if (input.equalsIgnoreCase("exit")) {
-        break;
-      }
-      System.out.println("From Client: " + input);
-      clientOut.println("Echo: " + input);
     }
     System.out.println("End program");
-    clientOut.close();
-    in.close();
-    fromclient.close();
-    servers.close();
   }
 }
